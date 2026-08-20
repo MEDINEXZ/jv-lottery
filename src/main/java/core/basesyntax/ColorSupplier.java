c622e2ff56987f9be80a1c3616a9bf9875efba8b
T-14M <step11122006@gmail.com>
2026-08-20 09:49:08 +0200

    Task done

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+
     public String getRandomColor() {
-        return null;
+        Color[] colors = Color.values();
+        Random random = new Random();
+
+        return colors[random.nextInt(colors.length)].toString();
     }
 }
